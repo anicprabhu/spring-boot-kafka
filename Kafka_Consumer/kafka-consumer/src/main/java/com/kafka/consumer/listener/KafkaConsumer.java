eeb9e29d2845c4ad51eeddbaaafb26dd180e4f0e
Anic <prabhu.anic@gmail.com>
2020-05-27 23:22:14 +0530

    Consumer Files

---
+++ Kafka_Consumer/kafka-consumer/src/main/java/com/kafka/consumer/listener/KafkaConsumer.java
@@ -0,0 +1,23 @@
+package com.kafka.consumer.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    
+    @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+    public void consume(String message){
+        System.out.println("Consumed Message"+message);
+
+    }
+
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json", )
+    public void consumeJson(String message){
+        System.out.println("Consumed Message"+message);
+
+    }
+
+    @KafkaListener(topics)
+
+}
